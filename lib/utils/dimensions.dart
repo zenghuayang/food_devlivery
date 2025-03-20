0f1105314c87aa69c930259a29bfd1ded8bb6d7b
Million <zenghuayang147@qq.com>
2025-03-20 08:40:02 +0800

    222

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -11,7 +11,14 @@ class Dimensions {
   static double heigh10 = screenHeight / 84.4;
   static double heigh15 = screenHeight / 56.27;
   static double heigh20 = screenHeight / 42.2;
+  static double heigh30 = screenHeight / 28.13;
   static double heigh45 = screenHeight / 18.75;
+
+  static double widht10 = screenHeight / 84.4;
+  static double widht15 = screenHeight / 56.27;
+  static double widht20 = screenHeight / 42.2;
+  static double widht30 = screenHeight / 28.13;
+  static double widht45 = screenHeight / 18.75;
 
   static double font16 = screenHeight / 52.75;
   static double font20 = screenHeight / 42.2;
